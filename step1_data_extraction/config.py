75713c3b09e0cdfb69905dbe8e684c1c4c9a8cad
omarlammari6-prog <omarlammari6@gmail.com>
2026-01-21 18:19:28 +0100

    Create config.py
    Ajouter le fichier config pour Chambéry

---
+++ step1_data_extraction/config.py
@@ -0,0 +1,11 @@
+# Bounding box format: (north, west, south, east)
+# City: Chambéry, France
+BBOX = (45.70, 5.75, 45.44, 6.05)
+
+# Time period
+START_DATE = "2023-01-01"
+END_DATE   = "2023-01-31"
+
+# Output paths
+CAMS_OUT = "../data/raw/cams_radiation.nc"
+ERA5_OUT = "../data/raw/era5_temperature.nc"
